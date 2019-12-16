09ab62f81a364753c3b79c82829cc26f63a3a3e5
unknown <gsharma172@gmail.com>
2019-12-16 15:28:02 +0530

    FirstPush

---
+++ go-stack/stack/stack.go
@@ -0,0 +1,73 @@
+package stack
+
+import "fmt"
+
+type stack struct {
+	size int
+	top int
+	slc []interface{}
+}
+
+//Creates a stack of given size with top initialized to -1
+func CreateStackUsingArray(size int) *stack{
+	return &stack{
+		size: size,
+		top:  -1,
+		slc:  make([]interface{}, size),
+	}
+}
+
+func (stk *stack) PushArrStack(data interface{}) {
+	if !stk.IsFull() {
+		stk.top++
+		stk.slc[stk.top] = data
+		return
+	}
+	fmt.Println("Stack Overflow! Stack is full")
+}
+
+//Removes top element from stack
+//element will be there in the slice, only top will be decremented, element will be replaced by other element when a new
+//element comes
+func (stk *stack) PopArrStack() {
+	if !stk.IsEmpty() {
+		stk.top--
+		return
+	}
+	fmt.Println("Stack Underflow! No element to delete")
+}
+
+//return top element of the stack
+func (stk *stack) TopArrStack() interface{} {
+	return stk.slc[stk.top]
+}
+
+//return given element from the stack, starting from 0 to size-1 , if not found returns nil, false
+func (stk *stack) Peek(pos int) (interface{}, bool) {
+	if pos > stk.size || stk.top == -1 {
+		return nil, false
+	}
+	return stk.slc[stk.top - pos + 1], true
+}
+
+func (stk *stack) IsEmpty() bool {
+	if stk.top == -1 {
+		return true
+	}
+	return false
+}
+
+func (stk *stack) IsFull() bool {
+	if stk.top == stk.size - 1 {
+		return true
+	}
+	return false
+}
+
+
+func DisplayArrStack(stk *stack) {
+	for i := stk.top; i >= 0; i-- {
+		fmt.Println(stk.slc[i])
+	}
+}
+
---
+++ go-stack/stack/stacklinkedlist.go
@@ -0,0 +1,90 @@
+package stack
+
+import "fmt"
+
+type node struct {
+	data interface{}
+	next *node
+}
+
+type lStack struct {
+	Top *node
+	len int //Denotes current number of elements in the stack
+	size int //Total number of elements the stack can accommodate
+}
+
+/*CreateStack creates an empty stack which can accommodate n nodes.
+  Initially the length len of stack will be zero and Top pointer will point to nil.
+*/
+func CreateStack(n int) *lStack {
+	return &lStack{
+		Top:  nil,
+		len:  0,
+		size: n,
+	}
+}
+
+func(stk *lStack) Push(data interface{}) {
+	if !stk.IsFull() {
+		newNode := &node{
+			data: data,
+			next: nil,
+		}
+		newNode.next = stk.Top
+		stk.Top = newNode
+		stk.len++
+		return
+	}
+	fmt.Println("StackOverflow!")
+}
+
+func(stk *lStack) Pop() {
+	if !stk.IsEmpty() {
+		temp := stk.Top.next
+		stk.Top.next = nil
+		stk.Top = temp
+		stk.len--
+		return
+	}
+	fmt.Println("StackUnderFlow!")
+}
+
+func (stk *lStack) GetTop() interface{} {
+	if !stk.IsEmpty() {
+		return stk.Top.data
+	}
+	return nil
+}
+
+func (stk *lStack) Peek(pos int) (interface{}, bool) {
+	temp := stk.Top
+	for i := 0; temp != nil && i < pos - 1; i++ {
+		temp = temp.next
+	}
+	if temp != nil {
+		return temp.data, true
+	}
+	return nil, false
+}
+
+func (stk *lStack) IsFull() bool {
+	//Limiting the number of elements in stack by size
+	if stk.size == stk.len {
+		return true
+	}
+	return false
+}
+
+func (stk *lStack) IsEmpty() bool {
+	if stk.Top == nil {
+		return true
+	}
+	return false
+}
+
+func Display(top *node) {
+	for top != nil {
+		fmt.Println(top.data)
+		top = top.next
+	}
+}
